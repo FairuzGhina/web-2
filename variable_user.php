b9f90baafd1c9a4327c735c04c67fe1a8253c614
FairuzGhina <fairuzgame39@gmail.com>
2025-02-20 12:14:10 +0700

    praktikum 1

---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinisikan variable
+$nama = "Ghina";
+$umur = 19;
+$berat = 50.30;
+
+echo "Nama saya " . $nama;
+echo "<br/>Umur : " . $umur . " Tahun";
+echo "<br/>Berat : " . $berat;
+
+?>
