3de0b7a30202119ab47a876d2f0959785f0505c7
FairuzGhina <fairuzgame39@gmail.com>
2025-06-23 10:44:55 +0700

    Praktikum 03

---
+++ partials/section/about.php
@@ -0,0 +1,17 @@
+<section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">About Me</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p class="lead">Halo! saya Fairuz Ghina Mufidah akrab dipanggil Ghina. Saya seorang Mahasiswa Sistem Informasi yang memiliki ketertarikan besar pada dunia teknologi, desain, dan pengembangan digital. Saya percaya bahwa teknologi bukan hanya tentang kode, tapi juga tentang bagaimana menciptakan solusi yang berdampak dan bermanfaat bagi banyak orang.</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">Saya memulai perjalanan di dunia teknologi dan desain sejak 2024, di mana saya memasuki kuliah. Di sana saya menemukan banyak hal baru yang sebelumnya belum saya temukan. Terima kasih telah berkunjung, dan saya harap kita bisa terhubung untuk berkolaborasi atau sekadar bertukar ide!</p></div>
+                </div>
+            </div>
+        </section>
